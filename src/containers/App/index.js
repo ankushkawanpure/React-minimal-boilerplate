3394a082b402dec57fef54b3c14bc373fa201d0f
ankush <ankushkawanpure@gmail.com>
2017-04-23 00:30:44 -0700

    All add

---
+++ src/containers/App/index.js
@@ -0,0 +1,23 @@
+import React, { Component, PropTypes } from 'react';
+
+// import Header from 'components/Header';
+// import Grid from 'components/Grid';
+import './style.css';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+
+                <div className="App-header">
+
+                    <h2>Ankush</h2>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ankush on 4/22/17.
+ */
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import App from './containers/App';
+import 'index.html';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
